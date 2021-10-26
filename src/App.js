bf1f94619c843ea3eb0889d0d7e8f1c5bec36bc7
Nicholas <nickycrandall@gmail.com>
2021-10-25 22:53:23 -0400

    added blog component, successfully fetches from the backend

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import './App.css';
 import React, { Component } from 'react'
 import NavBar from './components/NavBar'
+import Blog from './components/Blog'
 
-// let baseURL = '' 
-// if (process.env.NODE_ENV === 'development'){
-//   baseURL = process.env.REACT_APP_LOCAL_URL
-// } else {
-//   baseURL = process.env.REACT_APP_PROD_URL
-// }
+let baseURL = '' 
+if (process.env.NODE_ENV === 'development'){
+  baseURL = process.env.REACT_APP_LOCAL_URL
+} else {
+  baseURL = process.env.REACT_APP_PROD_URL
+}
 
 class App extends Component {
   constructor(props) {
@@ -22,6 +23,7 @@ class App extends Component {
       <div className="App">
         <NavBar />
         <h1>Home Page</h1>
+        <Blog baseURL={baseURL} />
       </div>
     )
   }
