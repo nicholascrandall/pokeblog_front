b19834bd1c628a768b1a682145742bbd35fd8a83
Nicholas <nickycrandall@gmail.com>
2021-10-26 21:50:59 -0400

    added a comma and space between every caught pokemon

--- src/components/BlogShow.js
+++ src/components/BlogShow.js
@@ -14,12 +14,13 @@ export default class BlogShow extends Component {
         if(!this.props.currentBlog){
             return <Redirect to='/' />
         } else {
+            const pokeList = this.props.currentBlog.caughtPokemon.join(', ')
         return (
             <div className="blogshow">
                 <h1>{this.props.currentBlog.name}'s PokeBlog</h1>
                 <h3>{this.props.currentBlog.about}</h3>
                 <img src={avatar} alt="avatar" />
-                <h3>Pokemon I've caught so far: {this.props.currentBlog.caughtPokemon} </h3>
+                <h3>Pokemon I've caught so far: {pokeList} </h3>
             </div>
         )
     }
