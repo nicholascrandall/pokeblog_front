b4378bbb86b366689b2ea0d929ff3bef863a1390
Nicholas <nickycrandall@gmail.com>
2021-10-26 22:23:29 -0400

    added backend blog creation to form page

--- src/components/BlogForm.js
+++ src/components/BlogForm.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Redirect } from 'react-router-dom'
 
 export default class BlogForm extends Component {
     constructor(props) {
@@ -8,7 +9,42 @@ export default class BlogForm extends Component {
         }
     }
 
+    handleChange = (event) => {
+        this.setState({
+            [event.target.id]: event.target.value
+        })
+    }
+
+    handleSubmit (event) {
+        event.preventDefault()
+
+        const url = this.props.baseURL + '/blog'
+
+        const blog = {
+            name: this.state.name,
+            about: this.state.about,
+            caughtPokemon: this.state.caughtPokemon
+        }
+
+        fetch(url, {
+            method: 'POST',
+            body: JSON.stringify({FormData: this.state.FormData}),
+            headers: {'Content-Type': 'application/json'},
+            mode: 'cors',
+            credentials: "include"
+        }).then(response => response.json())
+        .then(data => {
+            this.setState({
+                created: true
+            })
+        })
+    }
+
     render() {
+        console.log(this.state)
+        if (this.state.created) {
+            return <Redirect to='/' />
+        }
         return (
             <div className="blogForm">
                 <h3>Blog form page</h3>
