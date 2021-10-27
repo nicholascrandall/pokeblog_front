8866a636bd15a875e0d0f78cc1286de5687f7e6d
Nicholas <nickycrandall@gmail.com>
2021-10-26 21:57:43 -0400

    pokemon names are now capitalized

--- src/components/BlogShow.js
+++ src/components/BlogShow.js
@@ -14,13 +14,22 @@ export default class BlogShow extends Component {
         if(!this.props.currentBlog){
             return <Redirect to='/' />
         } else {
-            const pokeList = this.props.currentBlog.caughtPokemon.join(', ')
+            const capitalize = () => {
+                const pokeList = this.props.currentBlog.caughtPokemon.join(', ');
+                const words = pokeList.split(" ");
+
+                for(let i = 0; i < words.length; i++) {
+                    words[i] = words[i][0].toUpperCase() + words[i].substr(1)
+                }
+                return words.join(" ")
+            }
+            
         return (
             <div className="blogshow">
                 <h1>{this.props.currentBlog.name}'s PokeBlog</h1>
                 <h3>{this.props.currentBlog.about}</h3>
                 <img src={avatar} alt="avatar" />
-                <h3>Pokemon I've caught so far: {pokeList} </h3>
+                <h3>Pokemon I've caught so far: {capitalize()} </h3>
             </div>
         )
     }
